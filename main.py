76eeffe2211113c99e2e39d88a1f9d91f18a0110
gavrylova <it.gavrylova@gmail.com>
2023-04-26 08:29:53 +0300

    погода

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import telebot
-from telebot import types
+import requests, json
 
-bot = telebot.TeleBot("6125794533:AAHxwStb0LJZicd7OVcLUFXDYvwGSJ91oUc")
+API_KEY = "ca0cca3938bb8543f78af4199b823832"
+TELEGRAM_API_KEY = "6125794533:AAHxwStb0LJZicd7OVcLUFXDYvwGSJ91oUc"
+def weather(city, API_KEY):
+    BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+    URL = BASE_URL + "q=" + city + "&appid=" + API_KEY
+    response = requests.get(URL)
+    if response.status_code == 200:
+        data = response.json()
+        temperature = int(data['main']['temp'] - 273.15)
+        return temperature
+    else:
+        print("Error in HTTP request")
+
+
+bot = telebot.TeleBot(TELEGRAM_API_KEY)
+
 
 @bot.message_handler(commands=['start'])
-def start_handler(message):
-    bot.reply_to(message, "Hello, I'm a recipe bot. What recipe would you like to find?")
+def start_message(message):
+    msg = bot.reply_to(message, "Здравствуйте! Я - бот показывающий текущую погоду \U0001F31E по городу.")
+    bot.send_message(message.chat.id, "Введите название города, чтобы продолжить")
+    bot.register_next_step_handler(msg, print_weather)
+
+
+def print_weather(message):
+    city = message.text
+    temperature = weather(city, API_KEY)
+    msg = bot.reply_to(message, f"Текущая температура: {temperature}°C")
+    bot.send_message(message.chat.id, "Введите название города, чтобы продолжить")
+    bot.register_next_step_handler(msg, print_weather)
+
 
 bot.polling()
